cc78775767e7c049affebcd6ba98788249ccbb50
adityasriv2317 <adityaxia9237@gmail.com>
2024-12-03 16:46:21 +0530

    'merge'

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
+import React from "react";
+import Header from "./components/Header";
+import Landing from "./components/Landing";
+import About from "./components/About";
+import Registration from "./components/Registration";
+import Contact from "./components/Contact";
+
+const App = () => {
+  return (
+    <div className="text-white font-poppins overflow-x-hidden">
+      <Header />
+      <Landing />
+
+      {/* <About /> */}
+      <About />
+
+      {/* Additional Sections */}
+      <div className="about-section w-[100vw] h-screen bg-gray-600 flex items-center justify-center">
+        <h1 className="text-4xl">Team Section</h1>
+      </div>
+      <div className="services-section w-[100vw] h-screen bg-gray-700 flex items-center justify-center">
+        <h1 className="text-4xl">Mentor Section</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
 
 // import React from 'react'
 // import Carousel from './components/Carousel'
@@ -13,31 +41,3 @@
 //     </>
 //   )
 // }
-import React from "react";
-import Header from "./components/Header";
-import Landing from "./components/Landing";
-// import About from "./components/About";
-// import About from "./components/About";
-import Registration from "./components/Registration";
-import Contact from "./components/Contact";
-
-const App = () => {
-  return (
-    <div className="text-white font-poppins overflow-x-hidden">
-      <Header />
-      <Landing />
-
-      {/* <About /> */}
-
-      {/* Additional Sections */}
-      <div className="about-section w-[100vw] h-screen bg-gray-600 flex items-center justify-center">
-        <h1 className="text-4xl">Team Section</h1>
-      </div>
-      <div className="services-section w-[100vw] h-screen bg-gray-700 flex items-center justify-center">
-        <h1 className="text-4xl">Mentor Section</h1>
-      </div>
-    </div>
-  );
-};
-
-export default App;
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,4 @@
-
-
-
-
-
-
 import React, { useState } from "react";
-// import learn from "../images/learn.png";
 import learn from "../images/learn.jpg";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -57,7 +50,7 @@ const Carousel = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3, 
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
@@ -70,16 +63,16 @@ const Carousel = () => {
         },
       },
       {
-        breakpoint: 768, 
+        breakpoint: 768,
         settings: {
           slidesToShow: 1,
         },
       },
       {
-        breakpoint: 480, 
+        breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          dots: false, 
+          dots: false,
         },
       },
     ],
@@ -87,7 +80,7 @@ const Carousel = () => {
 
   return (
     <div className="mt-28 px-4 md:px-8 lg:px-16 relative">
-        <div className=" absolute bg-white rounded-2xl  bg-opacity-20 backdrop-blur-sm z-40 top-4 h-[330px] w-7/12 left-[635px] "></div>
+      <div className=" absolute bg-white rounded-2xl  bg-opacity-20 backdrop-blur-sm z-40 top-4 h-[330px] w-7/12 left-[635px] "></div>
       <Slider {...settings}>
         {obj.map((card, index) => (
           <div
@@ -96,31 +89,29 @@ const Carousel = () => {
               index === activeSlide ? "scale-110" : "scale-100 "
             }`}
           >
-          
-           
             {/* Text Section */}
             <div className="flex justify-center items-center text-center">
-            <div className="text flex-1 text-center md:text-left flex justify-center items-center flex-col">
-              <div className="name text-2xl md:text-3xl  text-white font-bolder">
-                {card.name}
+              <div className="text flex-1 text-center md:text-left flex justify-center items-center flex-col">
+                <div className="name text-2xl md:text-3xl  text-white font-bolder">
+                  {card.name}
+                </div>
+                <div className="surname text-3xl md:text-2xl text-white font-bolder">
+                  {card.surName}
+                </div>
+                <div className="designation text-2xl md:text-xl text-violet-400 mt-2 font-bolder">
+                  {card.designation}
+                </div>
               </div>
-              <div className="surname text-3xl md:text-2xl text-white font-bolder">
-                {card.surName}
-              </div>
-              <div className="designation text-2xl md:text-xl text-violet-400 mt-2 font-bolder">
-                {card.designation}
-              </div>
-            </div>
 
-            {/* Image Section */}
-            <div className="img w-40 h-40 md:w-64 md:h-80 flex-shrink-0 rounded-lg overflow-hidden shadow-md">
-              <img
-                src={card.img}
-                className="object-cover w-full h-full"
-                alt={`${card.name} ${card.surName}`}
-              />
+              {/* Image Section */}
+              <div className="img w-40 h-40 md:w-64 md:h-80 flex-shrink-0 rounded-lg overflow-hidden shadow-md">
+                <img
+                  src={card.img}
+                  className="object-cover w-full h-full"
+                  alt={`${card.name} ${card.surName}`}
+                />
+              </div>
             </div>
-          </div>
           </div>
         ))}
       </Slider>
