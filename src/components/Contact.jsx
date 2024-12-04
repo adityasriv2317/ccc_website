7222103b9ebd9c2b00b10c9ca910b4a4a6950837
adityasriv2317 <adityaxia9237@gmail.com>
2024-12-04 22:24:49 +0530

    'contact'

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,32 +1,3 @@
-
-
-
-// import React from 'react';
-// import invert from "../assets/invert.png";
-
-// const Contact = () => {
-//   return (
-//     <div id='contact'>
-//       <div className='text-white mt-32 flex justify-center items-center flex-col bg-red-400 w-4/12 h-auto py-5 rounded-md '>
-//       <h1 className='text-4xl text-start block'>Let's connect Networks</h1>
-//         <form action="">
-//         <div>
-//           <input type="text" className='border-white border-2 rounded-md font-bolder px-3 text-white py-2 mt-4 bg-gray-900' placeholder='First Name' /> 
-//           <input className='border-white border-2 rounded-md font-bolder px-3 text-white py-2 bg-gray-900 mt-4' type="text" placeholder='Last Name'/>
-//         </div>
-//         </form>
-//       </div>
-//       <div>
-//         <img src={invert} className="w-screen h-72" alt="Background" />
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Contact;
-
-
-
 import React from 'react';
 import invert from "../assets/invert.png";
 
