c1605832d18c6766622d0aba327b5175c60ba228
adityasriv2317 <adityaxia9237@gmail.com>
2024-12-04 16:26:05 +0530

    'favicon'

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import Header from "./components/Header";
 import Landing from "./components/Landing";
-import About from "./components/About";
+import Mentor from "./components/Mentor";
 import Registration from "./components/Registration";
 import Contact from "./components/Contact";
-<<<<<<< HEAD
+import About from "./components/About";
 // import Carousel from './components/Carousel'
 
-=======
-import Mentor from "./components/Mentor";
->>>>>>> 286606c2bfce9977e97750a2abb382970c8c64d1
 
 const App = () => {
   return (
     <div className="text-white font-poppins overflow-x-hidden">
       <Header />
       <Landing />
+      <About />
+      <Mentor />
 
       {/* <About /> */}
-<<<<<<< HEAD
       <Contact></Contact>
       <Registration/>
       {/* <Carousel></Carousel> */}
@@ -29,10 +27,6 @@ const App = () => {
       <div className="services-section w-[100vw] h-screen bg-gray-700 flex items-center justify-center">
         <h1 className="text-4xl">Mentor Section</h1>
       </div>
-=======
-      <About />
-      <Mentor />
->>>>>>> 286606c2bfce9977e97750a2abb382970c8c64d1
     </div>
   );
 };
