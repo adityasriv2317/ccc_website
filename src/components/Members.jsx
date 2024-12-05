1cb6e21c37072d33a09e24b0ab84720d10d6e5fb
adityasriv2317 <adityaxia9237@gmail.com>
2024-12-05 09:35:50 +0530

    'data'

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import tod from "../assets/about/tod.jpg";
 
 // imgs
 import akhand from "../assets/team/akhandSingh.jpg";
@@ -11,66 +10,40 @@ import shreeya from "../assets/team/shreeyaAggarwal.jpg";
 import sachendra from "../assets/team/sachendraGangwar.jpg";
 import prakhar from "../assets/team/prakharSrivastava.jpg";
 import kratika from "../assets/team/kratikaGupta.jpg";
+import sahil from "../assets/team/sahil.jpg";
 import kritika from "../assets/team/kritika.jpg";
 import harsh from "../assets/team/harshSingh.jpeg";
 import manoj from "../assets/team/manoj.jpg";
 import akshat from "../assets/team/akshatSingh.jpg";
 import aditya from "../assets/team/adityaKumar.jpeg";
 
+import taru from "../assets/team/final/taruPathak.jpg";
+import priya from "../assets/team/final/priya.jpg";
+import raghav from "../assets/team/final/raghav.jpg";
+import sarthak from "../assets/team/final/sarthak.jpg";
+import devrat from "../assets/team/final/devrat.jpg";
+import atul from "../assets/team/final/atul.jpg";
+import anjani from "../assets/team/final/anjani.jpg";
+import arpit from "../assets/team/final/arpit.png";
+import kartik from "../assets/team/final/kartik.jpg";
+import rishabh from "../assets/team/final/rishabh.jpg";
+import shrasti from "../assets/team/final/shrasti.jpg";
+import sonam from "../assets/team/final/sonam.jpg";
+import saurabh from "../assets/team/final/saurabh.jpg";
+import shashank from "../assets/team/final/shashank.jpg";
+
 const obj = [
+  {
+    name: "Aditya",
+    surName: "Kumar",
+    designation: "Backend Developer",
+    img: aditya,
+  },
   {
     name: "Akhand",
     surName: "Pratap Singh",
     designation: "Backend Developer",
     img: akhand,
-  },
-  {
-    name: "Prakhar",
-    surName: "Srivastava",
-    designation: "App Developer",
-    img: prakhar,
-  },
-  {
-    name: "Arpita",
-    surName: "Asthana",
-    designation: "Backend Developer",
-    img: arpita,
-  },
-  {
-    name: "Yash",
-    surName: "Mishra",
-    designation: "Frontend Developer",
-    img: yash,
-  },
-  {
-    name: "Kritika",
-    surName: "Agrawal",
-    designation: "ML Developer",
-    img: kritika,
-  },
-  {
-    name: "Sachendra",
-    surName: "Gangwar",
-    designation: "Frontend Developer",
-    img: sachendra,
-  },
-  // {
-  //   name: "Sahil",
-  //   surName: "Tomar",
-  //   designation: "ML Developer",
-  //   img: tod,
-  // },
-  {
-    name: "Manoj",
-    surName: "Samantha",
-    designation: "UI/UX Designer",
-    img: manoj,
-  },
-  {
-    name: "Diya",
-    surName: "Sirohi",
-    designation: "Cloud Developer",
-    img: diya,
   },
   {
     name: "Akshat",
@@ -79,10 +52,52 @@ const obj = [
     img: akshat,
   },
   {
+    name: "Anjani",
+    surName: "Singhal",
+    designation: "Frontend Developer",
+    img: anjani,
+  },
+  {
+    name: "Arpit",
+    surName: "Malik",
+    designation: "ML Developer",
+    img: arpit,
+  },
+  {
+    name: "Arpita",
+    surName: "Asthana",
+    designation: "Backend Developer",
+    img: arpita,
+  },
+  {
+    name: "Atul",
+    surName: "Kumar",
+    designation: "Frontend Developer",
+    img: atul,
+  },
+  {
+    name: "Dev Brat",
+    surName: "Tripathi",
+    designation: "ML Developer",
+    img: devrat,
+  },
+  {
+    name: "Diya",
+    surName: "Sirohi",
+    designation: "Cloud Developer",
+    img: diya,
+  },
+  {
     name: "Harsh",
     surName: "Singh",
     designation: "App Developer",
     img: harsh,
+  },
+  {
+    name: "Kartik",
+    surName: "Tyagi",
+    designation: "Frontend Developer",
+    img: kartik,
   },
   {
     name: "Kratika",
@@ -91,10 +106,76 @@ const obj = [
     img: kratika,
   },
   {
-    name: "Aditya",
-    surName: "Kumar",
-    designation: "Backend Developer",
-    img: aditya,
+    name: "Kritika",
+    surName: "Agrawal",
+    designation: "ML Developer",
+    img: kritika,
+  },
+  {
+    name: "Manoj",
+    surName: "Samantha",
+    designation: "UI/UX Designer",
+    img: manoj,
+  },
+  {
+    name: "Prakhar",
+    surName: "Srivastava",
+    designation: "App Developer",
+    img: prakhar,
+  },
+  {
+    name: "Priya",
+    surName: "Sharma",
+    designation: "Frontend Developer",
+    img: priya,
+  },
+  {
+    name: "Raghav",
+    surName: "Aggarwal",
+    designation: "App Developer",
+    img: raghav,
+  },
+  {
+    name: "Rishabh",
+    surName: "Gupta",
+    designation: "Frontend Developer",
+    img: rishabh,
+  },
+  {
+    name: "Sachendra",
+    surName: "Gangwar",
+    designation: "Frontend Developer",
+    img: sachendra,
+  },
+  {
+    name: "Sahil",
+    surName: "Tomar",
+    designation: "ML Developer",
+    img: sahil,
+  },
+  {
+    name: "Sarthak",
+    surName: "Srivastava",
+    designation: "Frontend Developer",
+    img: sarthak,
+  },
+  {
+    name: "Saurabh",
+    surName: "",
+    designation: "Frontend Developer",
+    img: saurabh,
+  },
+  {
+    name: "Shashank",
+    surName: "Singh",
+    designation: "App Developer",
+    img: shashank,
+  },
+  {
+    name: "Shreya",
+    surName: "Mohan",
+    designation: "Cloud Developer",
+    img: shreya,
   },
   {
     name: "Shreeya",
@@ -103,10 +184,28 @@ const obj = [
     img: shreeya,
   },
   {
-    name: "Shreya",
-    surName: "Mohan",
-    designation: "Cloud Developer",
-    img: shreya,
+    name: "Shrasti",
+    surName: "Yadav",
+    designation: "Frontend Developer",
+    img: shrasti,
+  },
+  {
+    name: "Sonam",
+    surName: "Kumari",
+    designation: "Frontend Developer",
+    img: sonam,
+  },
+  {
+    name: "Taru",
+    surName: "Pathak",
+    designation: "Frontend Developer",
+    img: taru,
+  },
+  {
+    name: "Yash",
+    surName: "Mishra",
+    designation: "Frontend Developer",
+    img: yash,
   },
 ];
 
